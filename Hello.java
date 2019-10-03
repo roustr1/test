4e44239f9df007e9618acec1a0d7b4cd6aa468d1
userB <you@example.com>
2019-10-03 11:34:52 +0300

    edit input

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
     public class Hello {
       public static void main(String[] args)
      {
-       System.out.println("B");
-       System.out.println("A");
+       System.out.println("Hello World2");
      }
     }
